3d0398f77a0393d93efb76ed66643408401b2ee3
anuragpatil811 <anuragpatil811@gmail.com>
2025-07-13 20:51:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+//console.log("Hello Anurag!")
+//alert("JARVIS is now active!");
+//console.log("hello");
+//window.console.log("Hello2");
+//window.alert('HELLO');
+/*console.dir(document.body);
+console.log(document.body);
+console.log(document.head);
+document.body.childNodes[3].innerText = "abcd"; */
+//console.dir(document.body);
+/*let heading = document.getElementById("heading"); //h1
+console.log(heading)
+console.dir(heading) */
+/*let button = document.getElementById("myId"); //h1
+console.dir(button)
+let headings = document.getElementsByClassName("myClass")
+console.dir(headings);
+console.log(headings); */
+/*let parahs = document.getElementsByTagName("p");
+console.dir(parahs); */
+
+/*let elements = document.querySelector(".myClass");
+console.dir(elements);
+let allEl = document.querySelectorAll(".myClass");
+console.dir(allEl); */
+/*let elements = document.querySelector(".myClass");
+console.dir(elements);
+let allEl = document.querySelectorAll(".myClass");
+console.dir(allEl); */
+/*let elements = document.querySelector("#myId");
+console.dir(elements);
+let allEl = document.querySelectorAll("#myId");
+console.dir(allEl);  */
+//console.dir(document.body.firstChild);
+/*let div= document.querySelector("div");
+console.dir(div);
+let heading = document.querySelector("h1");
+console.dir(heading); */
+ /*let h2 = document.querySelector("h2");
+console.dir(h2.innerText);
+
+//h2.innerText = 'abc';
+h2.innerText = h2.innerText + "from Apna College students"; //concatenate
+*/
+/*let divs = document.querySelectorAll(".box");
+let idx = 1;
+console.log(divs[0]);
+for (div of divs)
+{
+  console.log(div.innerText);
+  div.innerText = 'new unique value ${idx}';
+  idx++;
+}
+divs[0].innerText = "new unique value 1";
+divs[1].innerText = "new unique value 2";
+divs[2].innerText = "new unique value 3"; */
+/*let div = document.querySelector("div");
+console.log(div);
+let id = div.getAttribute("id");
+console.log(id);
+let name = div.getAttribute("name");
+console.log(name); */
+/*let para = document.querySelector("p");
+//console.log(para.getAttribute("class"));
+console.log(para.setAttribute("class", "newClass")); */
+/*let div = document.querySelector("div");
+//console.log(div)
+div.style.backgroundColor = "green";
+div.style.backgroundColor = "purple";
+//div.style.visibility = "hidden";
+div.style.fontSize = "26px";
+div.innerText = "Hello!";
+*/
+/*let newBtn = document.createElement("button");
+newBtn.innerText = "click me";
+console.log(newBtn);
+let div = document.querySelector("div");
+//div.append(newBtn);
+//div.prepend(newBtn);
+//div.before(newBtn);
+//div.after(newBtn);
+let p = document.querySelector("p");
+p.after(newBtn) */
+/*let newHeading = document.createElement("h1");
+newHeading.innerHTML = "<i>Hi, I am new!</i>";
+document.querySelector("body").prepend(newHeading);  
+let para = document.querySelector("p");
+para.remove();
+newHeading.remove(); */
+/*let newBtn = document.createElement("button");
+newBtn.innerText = "click me";
+newBtn.style.color = "white";
+newBtn.style.backgroundColor = "red";
+console.log(newBtn);
+document.querySelector("body").prepend(newBtn);  */
+
+//let para = document.querySelector("p");
