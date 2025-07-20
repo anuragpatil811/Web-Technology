b37b8a53d8f914f830a6686949281e0c960f2e9a
anuragpatil811 <anuragpatil811@gmail.com>
2025-07-21 00:15:38 +0530

    Add files via upload

--- Events.js
+++ Events.js
@@ -6,21 +6,61 @@ let btn1 = document.querySelector("#btn1");
     a++;
     console.log(a); //26
 }; */
-btn1.onclick = (evt) => {
+/*btn1.onclick = (evt) => {
     console.log(evt);
     console.log(evt.type);
     console.log(evt.target);
     console.log(evt.clientX, evt.clientY);
-};
+}; */
 
 /*let div = document.querySelector("div");
 div.onmouseover = () =>{
     console.log("you are inside div");
 }; */
-let div = document.querySelector("div");
-div.onmouseover = (evt) =>{
+//let div = document.querySelector("div");
+/*div.onmouseover = (evt) =>{
      console.log(evt);
     console.log(evt.type);
     console.log(evt.target);
     console.log(evt.clientX, evt.clientY);
-};
+}; */
+/*btn1.addEventListener("click", (evt) => {
+    //console.log("button 1 was clicked");
+    //console.log(evt);
+   // console.log(evt.type);
+      console.log("button 1 was clicked - handler1");
+});
+btn1.addEventListener("click", () => {
+    console.log("button1 was clicked - handler2");
+});
+const handler3 = () =>
+{
+    console.log("button1 was clicked - handler3");
+}
+btn1.addEventListener("click", handler3);
+btn1.addEventListener("click", () => {
+    console.log("button1 was clicked - handler4");
+});
+btn1.removeEventListener("click", handler3); */
+let modeBtn = document.querySelector("#mode");
+let body = document.querySelector("body");
+let currMode = "light"; 
+modeBtn.addEventListener("click", () => {
+   // console.log("you are trying to change mode");
+   if(currMode == "light")
+   {
+    currMode = "dark";
+   // document.querySelector("body").style.backgroundColor = "black";
+   //or
+    document.querySelector("body").classList.add("dark");
+    document.querySelector("body").classList.remove("light");
+   }
+   else{
+    currMode = "light";
+    //document.querySelector("body").style.backgroundColor = "white";
+    //or
+    body.classList.add("light");
+    body.classList.remove("dark");
+   }
+   console.log(currMode)
+})
